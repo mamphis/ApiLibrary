b0af1b32ac4cc34e445de8f078725b592787c7be
mamphis <10652205+mamphis@users.noreply.github.com>
2024-10-01 11:50:43 +0000

    refactor: update httpLogger middleware to include traceId in Response type

--- api-node/src/express/middleware/httpLogger.ts
+++ api-node/src/express/middleware/httpLogger.ts
@@ -3,14 +3,14 @@ import { Logger } from "../../util/logger";
 import { randomUUID } from 'crypto';
 import chalk from "chalk";
 
-export const httpLogger = (logger: Logger = Logger.createScopedLogger('server')) => (req: Request, res: Response, next: NextFunction) => {
+export const httpLogger = (logger: Logger = Logger.createScopedLogger('server')) => (req: Request, res: Response<any, { traceId: string }>, next: NextFunction) => {
     const start = Date.now();
     res.locals.traceId = randomUUID();
     res.set('ApiTraceId', res.locals.traceId);
     res.set('Access-Control-Expose-Headers', 'ApiTraceId');
     res.set('Access-Control-Allow-Headers', 'ApiTraceId');
 
-    res.on('finish', () => {
+    res.once('finish', () => {
         logger.debug(`${chalk.yellow(res.locals.traceId)} | ${req.method} ${req.originalUrl} ${res.statusCode} ${Date.now() - start}ms`);
     });
 
