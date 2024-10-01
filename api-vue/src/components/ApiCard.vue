1fa24f843e40bdcb4c2ee8979610d2fcece71daa
mamphis <10652205+mamphis@users.noreply.github.com>
2024-10-01 11:50:56 +0000

    refactor: update ApiCard component to include factboxes section

--- api-vue/src/components/ApiCard.vue
+++ api-vue/src/components/ApiCard.vue
@@ -15,13 +15,21 @@ const emits = defineEmits<{
 
 <template>
     <main>
-        <h1 class="title">{{ title }}</h1>
-        <div class="action-banner">
-            <slot name="actions"></slot>
-        </div>
-        <slot></slot>
-        <div class="buttons">
-            <button v-if="backVisible" @click="emits('back')">Back</button>
+        <div class="content">
+            <div class="main-content">
+                <h1 class="title">{{ title }}</h1>
+                <div class="action-banner" v-if="$slots.actions">
+                    <slot name="actions"></slot>
+                </div>
+                <slot></slot>
+                <div class="factboxes" v-if="$slots.factboxes">
+                    <slot name="factboxes"></slot>
+                </div>
+            </div>
+            <div class="buttons">
+                <button v-if="backVisible" @click="emits('back')">Back</button>
+                <slot name="buttons"></slot>
+            </div>
         </div>
     </main>
 </template>
@@ -36,5 +44,46 @@ const emits = defineEmits<{
 
 .action-banner :deep(button) {
     border-radius: 0px !important;
+}
+
+main {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+}
+
+.content {
+    display: flex;
+    flex: 1;
+    flex-direction: row;
+}
+
+.main-content {
+    display: flex;
+    flex-direction: column;
+
+    flex: 4;
+
+    padding: 1rem;
+}
+
+.factboxes {
+    display: flex;
+    flex-direction: column;
+
+    flex: 1;
+    padding: 1rem;
+}
+
+.factboxes:empty {
+    display: none;
+}
+
+.factboxes>* {
+    border: 1px solid var(--color-border);
+    margin-bottom: 1rem;
+    padding: 0.5rem 0.25rem;
+
+    border-radius: 0.25rem;
 }
 </style>
